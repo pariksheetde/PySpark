296e5edf573cacd1cae3aea582d8fdf469b3e871
Pariksheet De <41860297+pariksheetde@users.noreply.github.com>
2023-02-18 18:37:28 +0530

    DeltaLake commit

---
+++ DeltaLake/Test.py
@@ -0,0 +1,28 @@
+from pyspark.sql import *
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import StructType
+
+if __name__ == "__main__":
+    print("Invoice Data Analysis 1")
+
+spark = SparkSession.builder.appName("Invoice Data Analysis 1").master("local[3]").getOrCreate()
+
+invoice_df = (spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .option("dateFormat", "dd-MMM-yy")
+    # .option("mode", "PERMISSIVE")
+    .option("sep", ",")
+    .option("nullValue", "NA")
+    .option("compression", "snappy")
+    .csv("D:/DataSet/DataSet/SparkData/Invoices.csv"))
+
+invoice_df.printSchema()
+invoice_df.show(truncate = False)
+
+invoice_df.write.format("delta").mode('overwrite').save("D:/DataSet/OutputDataset/Delta/flight_data")
+
+spark.stop()
