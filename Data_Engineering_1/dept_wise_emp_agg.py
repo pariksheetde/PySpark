000574339bfefa5227a92c50b67d634b63d68628
pariksheet <depariksheet2@gmail.com>
2025-08-22 00:47:12 +0530

    COMMIT

--- Data_Engineering_1/dept_wise_emp_agg.py
+++ Data_Engineering_1/dept_wise_emp_agg.py
@@ -15,44 +15,71 @@ def create_employees_dataframe(spark):
     Returns:
         DataFrame: Customer DataFrame with pre-defined schema.
     """
-    schema = StructType([
-        StructField("Name", StringType(), True),
-        StructField("Date", IntegerType(), True),
-        StructField("Month", IntegerType(), True),
-        StructField("Year", IntegerType(), True)
-    ])
 
-    columns = ["Name", "Date", "Month", "Year"]
+    emp_columns = ["Emp_ID", "First_Name", "Last_Name", "Salary", "Manager_ID", "Dept_ID"]
 
-    data = [("Monica", "12", "1", "2002"),
-            ("Kate", "14", "10", "81"),
-            ("Peter", "25", "05", "63"),
-            ("Pamela", "16", "12", "06"),
-            ("Kylie", "19", "9", "85")
-            ]
+    emp_data = [(100, "Steve", "Smith", 2500012, None, 10),
+            (110, "Brian", "Lara", 2500147, 100, 10),
+            (120, "Steve", "Jobs", 1245000, 110, 10),
+            (130, "Peter", "Croft", 1000000, 120, 15),
+            (140, "Daren", "Walker", 1450000, 130, 15),
+            (150, "Brian", "Adams", 1750000, 140, 15),
+            (160, "Dwyane", "Jhonson", 2600000, 150, 20),
+            (170, "Monica", "Bellucci", 3125000, 160, 20),
+            (180, "Kate", "Moss", 3985000, 170, 20),
+            (190, "Emma", "Watson", 4500000, 180, 30),
+            (200, "Scarlett", "Johansson", 5000000, 190, 30),
+            (210, "Natalie", "Portman", 6000000, 200, 30),
+            (220, "Jennifer", "Lawrence", 7000000, 210, 30),
+        ]
 
-    emp_df = spark.createDataFrame(data=data, schema=columns)
-
-    # Convert columns to IntegerType
-    emp_df = emp_df.withColumn("Date", col("Date").cast(IntegerType())) \
-           .withColumn("Month", col("Month").cast(IntegerType())) \
-           .withColumn("Year", col("Year").cast(IntegerType()))
-
-    # Calculate Actual_Birth_Year
-    emp_df = emp_df.withColumn(
-        "Actual_Birth_Year",
-        when(col("Year") < 21, col("Year") + 2000)
-        .when(col("Year") < 100, col("Year") + 1900)
-        .otherwise(col("Year"))
-    )
-
-    # Create DOB column
-    emp_df = emp_df.withColumn(
-        "DOB",
-        to_date(expr("concat(Date, '/', Month, '/', Actual_Birth_Year)"), "d/M/y")
-    )
-
+    emp_df = spark.createDataFrame(data=emp_data, schema=emp_columns)
     return emp_df
+
+# This function defines data for departments.
+def create_departments_dataframe(spark):
+    """
+    Defines data for departments.
+    Args:
+        spark (SparkSession): The current SparkSession.
+    Returns:
+        DataFrame: Departments DataFrame with pre-defined schema
+    """
+    dept_schema = StructType([
+      StructField("dept_id", IntegerType(), True),
+      StructField("dept_name", StringType(), True)]
+    )
+    dept_columns = ["Dept_ID","Dept_Name"]
+
+    dept_data = [(10, "SQL Developer"),
+          (15, "PySpark Developer"),
+          (20, "Java Developer"),
+          (25, "ETL Developer"),
+          (30, "AWS Cloud Developer")
+          ]
+
+    dept_df = spark.createDataFrame(data=dept_data, schema=dept_columns)
+
+    return dept_df
+
+def create_emp_dept_agg_df(emp_df, dept_df):
+    """
+    Creates a DataFrame that aggregates employee data by department.
+    Args:
+        emp_df (DataFrame): DataFrame containing employee details.
+        dept_df (DataFrame): DataFrame containing department details.
+    Returns:
+        DataFrame: Aggregated DataFrame with employee count and average salary per department.
+    """
+    emp_dept_agg_df = emp_df.join(dept_df, emp_df.Dept_ID == dept_df.Dept_ID, "inner") \
+        .groupBy("Dept_Name") \
+        .agg(
+            count("Emp_ID").alias("Employee_Count"),
+            avg("Salary").alias("Avg_Salary")
+        ) \
+        .orderBy(desc("Employee_Count"))
+
+    return emp_dept_agg_df
 
 if __name__ == "__main__":
   print("=== Package: Data_Engineering_1 | Script: dept_wise_emp_agg ===")
@@ -68,9 +95,20 @@ if __name__ == "__main__":
 
   # Create and display DataFrame
   emp_df = create_employees_dataframe(spark)
+  dept_df = create_departments_dataframe(spark)
   print("DataFrame:")
 
+  print("=== Employees DataFrame ===")
   emp_df.printSchema()
   emp_df.select("*").show(n = 10, truncate=False)
+
+  print("=== Departments DataFrame ===")
+  dept_df.printSchema()
+  dept_df.select("*").show(n = 10, truncate=False)
+
+  print("=== Aggregated Employee Data by Department ===")
+  emp_dept_agg_df = create_emp_dept_agg_df(emp_df, dept_df)
+  emp_dept_agg_df.printSchema()
+  emp_dept_agg_df.select("*").show(emp_dept_agg_df.count(), truncate=False)
 
   spark.stop()
