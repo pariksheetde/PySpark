1ce71dd491e6a28681b8130daac10ae8e2d6c831
pariksheet <depariksheet2@gmail.com>
2025-08-30 21:25:40 +0530

    COMMIT - Data_Engineering_2departments_analysis_1.py

--- Data_Engineering_2/departments_analysis_1.py
+++ Data_Engineering_2/departments_analysis_1.py
@@ -5,26 +5,36 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import StructType
 
-if __name__ == "__main__":
-    print("Package : Data_Engineering_2, Script : Departments Analysis")
-
-spark = SparkSession.builder.appName("Departments_Analysis_1").master("local[3]").getOrCreate()
-
-dept_schema = StructType([
+def generate_department_analysis(spark):
+    
+    dept_schema = StructType([
     StructField("dept_id", IntegerType(), True),
     StructField("dept_name", StringType(), True),
     StructField("loc_id", IntegerType(), True)
-])
+    ])
 
-dept_df = spark.read \
+    dept_df = spark.read \
     .format("csv") \
     .schema(dept_schema) \
     .option("header", "true") \
     .load("D:/DataSet/DataSet/SparkDataSet/departments.csv")
 
-sel_dept = dept_df.selectExpr("dept_id as DeptID", "dept_name as Dept_Nm", "loc_id as LocID")
+    select_required_columns_df = dept_df.selectExpr("dept_id as DeptID", "dept_name as DeptName", "loc_id as LocID")
 
-sel_dept.printSchema()
-sel_dept.show(10, False)
+    return select_required_columns_df
 
-spark.stop()
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Departments Analysis 1").master("local[3]").getOrCreate()
+
+    # Suppress unnecessary Spark logging
+    spark.sparkContext.setLogLevel("ERROR")
+
+    result_df = generate_department_analysis(spark)
+    result_df.show(100, False)
+    print(f"Total Records Processed: {result_df.count()}")
+
+    # Suppress unnecessary Spark logging
+    spark.sparkContext.setLogLevel("ERROR")
+    
+    # Stop Spark session
+    spark.stop()
