a4fcdabed1aebbc04da10e50bcf20dbe9756be3a
pariksheet <depariksheet2@gmail.com>
2026-03-10 19:46:03 +0530

    COMMIT

---
+++ Airflow/dags/test.py
@@ -0,0 +1,23 @@
+# from airflow.sdk import dag, task
+from airflow.decorators import dag, task
+
+@dag(dag_id="first_dag"
+)
+def first_dag():
+    
+    @task
+    def first_task():
+        print("First Function")
+
+    @task
+    def second_task():
+        print("Second Function")
+
+    @task
+    def third_task():
+        print("Third Function")
+
+    first_task() >> second_task() >> third_task()
+
+# INITIALIZE DAG
+first_dag()
