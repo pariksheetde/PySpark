cd4e45658846434540ffdde58e6bf0aa70120147
pariksheet <depariksheet2@gmail.com>
2026-03-17 11:36:05 +0530

    COMMIT

--- Airflow/dags/02_operator.py
+++ Airflow/dags/02_operator.py
@@ -8,18 +8,18 @@ from airflow.operators.bash import BashOperator
 def DEEPSI_BASH_DAG():
     
     @task.python
-    def first_task():
+    def first_python_task():
         print("First Function")
 
     @task.python
-    def second_task():
+    def second_python_task():
         print("Second Function")
     
     @task.bash
     def bash_task():
         return "echo 'www.airflow.com'"
 
-    first_task() >> second_task() >> bash_task()
+    first_python_task() >> second_python_task() >> bash_task()
 
 # INITIALIZE DAG
 DEEPSI_BASH_DAG()
