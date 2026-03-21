2a72798da022c0f9dfc28a495de24ff18bf6d167
pariksheet <depariksheet2@gmail.com>
2026-03-21 11:51:39 +0530

    Modifying 06_second_paralle_task

--- Airflow/dags/06_second_parallel_task.py
+++ Airflow/dags/06_second_parallel_task.py
@@ -12,7 +12,7 @@ def PARALLEL_TASKS_DAG():
         ti = kwargs['ti']
         extracted_data_dict = {"framework" : ["SPARK", 'FLINK', 'RAY'],
                          "database" : ['SNOWFLAKE', 'BIGQUERY', 'REDSHIFT'],
-                         "programming Language" : ['SQL', 'PYTHON', 'JAVA', 'SCALA']}
+                         "programming_language" : ['SQL', 'PYTHON', 'JAVA', 'SCALA']}
         ti.xcom_push(key='return_value', value=extracted_data_dict)
 
 
@@ -22,30 +22,30 @@ def PARALLEL_TASKS_DAG():
         framwork_extracted = ti.xcom_pull(task_ids ='extract_task')['framework']
         print(f"Transforming {framwork_extracted} data")
         transform_framework = [framework.capitalize() for framework in framwork_extracted]
-        print(f"Transformed framework data: {transform_framework}")
-        # ti.xcom_push(key='return_value', value=transform_framework)
+        print(f"Transformed Framework: {transform_framework}")
+        ti.xcom_push(key='return_value', value=transform_framework)
 
-    # @task
-    # def transform_database(**kwargs):
-    #     ti = kwargs['ti']
-    #     database_extracted = ti.xcom_pull(task_ids ='extract_task')['database']
-    #     print(f"Transforming {database_extracted} data")
-    #     transform_database = [database.upper() for database in database_extracted]
-    #     print(f"Transformed database data: {transform_database}")
-    #     ti.xcom_push(key='return_value', value=transform_database)
+    @task
+    def transform_database(**kwargs):
+        ti = kwargs['ti']
+        database_extracted = ti.xcom_pull(task_ids ='extract_task')['database']
+        print(f"Transforming {database_extracted} data")
+        transform_database = [database.capitalize() for database in database_extracted]
+        print(f"Transformed Database: {transform_database}")
+        ti.xcom_push(key='return_value', value=transform_database)
 
-    # @task
-    # def transform_programming_language(**kwargs):
-    #     ti = kwargs['ti']
-    #     programming_language_extracted = ti.xcom_pull(task_ids ='extract_task')['programming_language']
-    #     print(f"Transforming {programming_language_extracted} data")
-    #     transform_programming_language = [language.upper() for language in programming_language_extracted]
-    #     print(f"Transformed programming language data: {transform_programming_language}")
-    #     ti.xcom_push(key='return_value', value=transform_programming_language)
+    @task
+    def transform_programming_language(**kwargs):
+        ti = kwargs['ti']
+        programming_language_extracted = ti.xcom_pull(task_ids ='extract_task')['programming_language']
+        print(f"Transforming {programming_language_extracted} data")
+        transform_programming_language = [language.capitalize() for language in programming_language_extracted]
+        print(f"Transformed Programming Language: {transform_programming_language}")
+        ti.xcom_push(key='return_value', value=transform_programming_language)
 
 
     # extract_task() >> [transform_framework(), transform_database(), transform_programming_language()]
-    extract_task() >> transform_framework()
+    extract_task() >> transform_framework() >> transform_database() >> transform_programming_language()
 
 # INITIALIZE DAG
 PARALLEL_TASKS_DAG()
